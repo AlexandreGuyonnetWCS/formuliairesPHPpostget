085ae92714d61d3af112b16854054bc7da7419f0
AlexandreGuyonnetWCS <alexandre.guyonnet.pro@gmail.com>
2022-04-06 09:15:48 +0200

    modification fonction

--- index.php
+++ index.php
@@ -58,6 +58,8 @@
       echo ("Merci" . $lastname . $name . "de nous avoir contacté à propos de" . $subjects . "." . PHP_EOL .
         "Un de nos conseiller vous contactera soit à l’adresse"  . $email . "ou par téléphone au" . $phone . "dans les plus brefs délais pour traiter votre demande :"
         . $message);
+        return ('Merci ' . $_POST['name'] . ' de nous avoir contacté à propos de ' . $_POST['subjects'] . '.' .'Un de nos conseiller vous contactera soit à l’adresse ' . $_POST['email'] . ' ou par téléphone au ' . $_POST['phone'] .' dans les plus brefs délais pour traiter votre demande : 
+        ' . $_POST['message'] . '.');
     }
   }
 
